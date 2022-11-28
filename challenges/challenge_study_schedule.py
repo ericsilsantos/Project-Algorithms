c034bee22c87a5e83a2dfa87b798045dcdd3bdf6
Eric Santos <eric.silvasantos44@gmail.com>
2022-11-28 09:30:35 -0300

    primeiro requisito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    users = 0
+    try:
+        for period in permanence_period:
+            if (period[0] <= target_time <= period[1]):
+                users +=1
+    except TypeError:
+        return None
+
+    return users
